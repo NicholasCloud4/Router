e141948f7cfb46a906ea47a3ec416515b74be8aa
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-02-24 14:28:39 -0500

    Using Links to navigate between Pages

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
     return (
-        <h1>Home Page</h1>
+        <>
+            <h1>Home Page</h1>
+            <p>Go to <Link to="/products">the list of products.</Link></p>
+        </>
     )
 }
