7a942274ecbe218010aa4a8884a1d85afe324487
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-02-24 14:12:32 -0500

    Added products route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
 
 // https://example.com/products
 
 const router = createBrowserRouter([
     { path: '/', element: <HomePage /> },
+    { path: '/products', element: <ProductsPage /> }
 ])
 
 function App() {
